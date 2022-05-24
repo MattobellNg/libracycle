fbc8674996d31799bb88b2608b2216de61bcbf26
Babatope Ajepe <ajepebabatope@gmail.com>
2022-05-24 13:23:08 +0100

    [UPD] update

--- project_description/__manifest__.py
+++ project_description/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Project Description",
     "summary": "Add a description to projects",
-    "version": "15.0.2.0.1",
+    "version": "2.0.1",
     "category": "Project",
     "website": "https://github.com/OCA/project",
     "author": "Tecnativa, " "C2i Change 2 improve, " "Odoo Community Association (OCA)",
