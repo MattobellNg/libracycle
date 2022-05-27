f4b0b066072cac82a8e098e88fbafda3674ba33f
Dhaval Taurani <dhaval.tr@vperfectcs.com>
2022-05-27 19:35:16 +0530

    [IMP] Added point BR-LIBPRJ-07

--- customize_vpcs/models/project.py
+++ customize_vpcs/models/project.py
@@ -1,6 +1,6 @@
 from odoo import models, fields, api
 
-class project_project(models.Model):
+class ProjectProject(models.Model):
 
     _inherit = "project.project"
 
@@ -11,3 +11,40 @@ class project_project(models.Model):
     )
     document = fields.Binary(required=True, attachment=False, help="upload here your document")
     container = fields.Integer(string="Number of Container")
+    status_delivered = fields.Boolean(string="Delivered")
+    status_completed = fields.Boolean(string="Completed")
+
+    @api.onchange('status_delivered')
+    def action_delivered(self):
+        for rec in self:
+            if rec.status_delivered == True:
+                rec.write({'state':'deliver'})
+            else:
+                rec.write({'state':'pending'})
+
+    @api.onchange('status_completed')
+    def action_completed(self):
+        for rec in self:
+            if rec.status_completed == True:
+                rec.write({'state':'done'})
+            else:
+                if rec.status_delivered == True:
+                    rec.write({'state':'deliver'})
+                else:
+                    rec.write({'state':'pending'})
+
+
+    @api.depends("analytic_account_id.credit", "analytic_account_id.debit")
+    def _compute_project_balance(self):
+        for rec in self:
+            rec.analysis_balance = rec.analytic_account_id.balance
+            if abs(rec.analytic_account_id.balance) > 0.0 and (rec.state == "pending" or not rec.state):
+                rec.state = "progress"
+            else:
+                if rec.status_delivered == True:
+                    if rec.status_completed == True:
+                        rec.state = 'done'
+                    else:
+                        rec.state = 'deliver'
+                else:
+                    rec.state = "pending"
