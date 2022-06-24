fe49cecf62dbefaeb9dcee642e5bfe16d5d965c1
Dhaval Taurani <dhaval.tr@vperfectcs.com>
2022-06-24 19:20:12 +0530

    [IMP] Report Changes

---
+++ customize_vpcs/report/waybill.py
@@ -0,0 +1,73 @@
+from tracemalloc import start
+from odoo import models
+from odoo.tools.misc import xlwt
+from datetime import datetime
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
+
+
+class WaybillXlsx(models.AbstractModel):
+    _name = 'report.customize_vpcs.report_waybill_xlsx'
+    _inherit = 'report.report_xlsx.abstract'
+
+    def generate_xlsx_report(self, workbook, data, items):
+        print('Items :', items)
+        headers = ["BL NO", "Job Ref No", "40FT", "20FT", "CBM", "KG", "ITEM DESCRIPTION", "SHIPPING LINE", "TERMINAL", "JOB DYNAMICS", "ATA", "TDO Date", "Delivery completion Date", "Final Destination", "Complete Doc. Received", "Duty", "Shipping charge", "Terminal Charge ", "NAFDAC",
+                   "SON", "Agency", "Transportation", "Others", "Total Cost(N)", "Duty", "Shipping charge", "Terminal Charge ", "NAFDAC", "Agency", "Transportation", "Others", "Invoice Value(N)", "VAT(N)", "Total Invoice Value(N)", "Paid(N)", "WHT(N)", "Unpaid(N)", "Total Profit(N)", "COMMENT"]
+        sheet = workbook.add_worksheet("waybill")
+        cell_format = workbook.add_format(
+            {"bold": True, "font_size": 8, "border": True, "align": 'left'})
+        header_format = workbook.add_format(
+            {"bold": True, "font_size": 8, "border": True, "align": 'center', })
+        sheet.merge_range(
+            0, 0, 0, 14, 'INVOICE SUBMISSION TRACKING REPORT - SPRINGFIELD AGRO', cell_format)
+        sheet.merge_range(
+            0, 15, 0, 23, 'EXPENSE CENTRES IN NAIRA(summation of inventory items in analytic/when amount double clicks it drills down to details/breakdown', cell_format)
+        sheet.merge_range(0, 24, 0, 36, 'INVOICE CENTRES IN NAIRA/DOLLAR/EURO/POUNDS(summation of inventory items and all income lines in validated invoice reporting in analytic/when amount double clicks it drills down to details/breakdown', cell_format)
+        sheet.merge_range(0, 37, 0, 38, 'Profile Section', cell_format)
+        row = 1
+        column = 0
+        for i in headers:
+            sheet.write(row,column,i,header_format)
+            sheet.set_column(0,column,15)
+            column+=1
+        for item in items:
+            pass
+
+class BargedXlsx(models.AbstractModel):
+    _name = 'report.customize_vpcs.report_barged_xlsx'
+    _inherit = 'report.report_xlsx.abstract'
+
+    def generate_xlsx_report(self, workbook, data, items):
+        print('Items :', items)
+        headers = ["S/N",'DATE','CONT NO','BL NO','SIZE','CLIENT','DESTINATION','FILE NO','WEIGHT','LINER']
+        sheet = workbook.add_worksheet("Barged_Tracking")
+        header_format = workbook.add_format(
+            {"bold": True, "font_size": 12, "border": True, "align": 'center',"bg_color":"#000080","color":'white'})
+        cell_format = workbook.add_format(
+            {"font_size": 8, "border": True, "align": 'center'})
+        row = 0
+        column = 0
+        for i in headers:
+            sheet.write(row,column,i,header_format)
+            column+=1
+        sr_no = 1
+        new_row = 0
+        for j in items:
+            new_row+=1
+            date_time = datetime.strptime(j.create_date.strftime("%Y-%m-%d"), '%Y-%m-%d')
+            sheet.write(new_row,0,sr_no,cell_format)
+            sheet.write_datetime(new_row,1,date_time,cell_format)
+            sheet.write(new_row,2,"",cell_format)
+            sheet.set_column(2,2,15)
+            sheet.write(new_row,3,j.project_id.bol_awb_ref,cell_format)
+            sheet.set_column(3,3,16)
+            sheet.write(new_row,4,j.project_id.items_total_size,cell_format)
+            sheet.write(new_row,5,j.project_id.partner_id.name,cell_format)
+            sheet.set_column(5,5,17)
+            sheet.write(new_row,6,"",cell_format)
+            sheet.set_column(6,6,30)
+            sheet.write(new_row,7,"",cell_format)
+            sheet.set_column(7,7,15)
+            sheet.write(new_row,8,j.project_id.items_total_weight,cell_format)
+            sheet.write(new_row,9,j.project_id.has_job_liner,cell_format)
+            sr_no+=1
