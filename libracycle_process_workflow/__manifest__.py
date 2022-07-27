5c09e4fd47153a41318fe62b470b027f83871373
Babatope Ajepe <ajepebabatope@gmail.com>
2022-07-27 09:55:19 +0100

    [ADD] add  libracycle_process_workflow

---
+++ libracycle_process_workflow/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Libra Payment Process Flow",
+    "summary": """Libra Payment Process Flow""",
+    "description": """
+        Libra Payment Process Flow
+    """,
+    "author": "Matt O'Bell",
+    "website": "http://www.yourcompany.com",
+    "version": "1.0",
+    "depends": ["base", "account_accountant", "purchase_requisition"],
+    "data": [
+        # 'security/ir.model.access.csv',
+        "security/res_group.xml",
+        "views/account_move.xml",
+        "views/templates.xml",
+    ],
+    "license": "LGPL-3",
+}
