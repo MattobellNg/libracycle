588eb819316e98461b0df21344a5cfd8cdb7d4a9
Babatope Ajepe <ajepebabatope@gmail.com>
2022-07-29 10:38:03 +0100

    [UPD] libracycle_payroll

--- libracycle_payroll/__manifest__.py
+++ libracycle_payroll/__manifest__.py
@@ -5,13 +5,14 @@
     'author': "Matt O'Bell",
     'website': "https://www.mattobell.net",
     'category': 'Uncategorized',
-    'version': '1..0.0',
+    'version': '1.1',
     'depends': ['base', 'hr_payroll'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/hr_contract.xml',
+        'views/hr_payslip.xml',
 
     ],
 
