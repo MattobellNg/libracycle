edf8477409cb4bc493fa9a39b383df6be7daf9b6
Babatope Ajepe <ajepebabatope@gmail.com>
2022-08-15 15:18:37 +0100

    [ADD] add libracycle_process_workflow

--- employee_advance_salary/__openerp__.py
+++ employee_advance_salary/__openerp__.py
@@ -3,14 +3,14 @@
 # Part of Probuse Consulting Service Pvt Ltd. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Employee Advance Salary Requests',
-    'version': '1.0',
-    'price': 49.0,
-    'currency': 'EUR',
-    'license': 'Other proprietary',
-    'category': 'Human Resources',
-    'summary': 'Employee Advance Salary Requests and Workflow - Integrated with Accounting',
-    'description': """
+    "name": "Employee Advance Salary Requests",
+    "version": "1.0",
+    "price": 49.0,
+    "currency": "EUR",
+    "license": "Other proprietary",
+    "category": "Human Resources",
+    "summary": "Employee Advance Salary Requests and Workflow - Integrated with Accounting",
+    "description": """
         Employee Advance Salary Requests:
 
 Employee advance salary
@@ -40,18 +40,19 @@ payroll employee
 employee hr payroll
 payroll
             """,
-    'author': 'Probuse Consulting Service Pvt. Ltd.',
-    'website': 'www.probuse.com',
-    'depends': ['hr', 'account', 'hr_payroll'],
-    'data': ['security/employee_advance_salary_security.xml',
-             'security/ir.model.access.csv',
-              'data/salary_rule_data.xml',
-             'views/employee_advance_salary.xml',
-             'views/hr_job.xml',
-             'report/employee_advance_salary_report.xml'
-             ],
-    'installable': True,
-    'application': False,
+    "author": "Probuse Consulting Service Pvt. Ltd.",
+    "website": "www.probuse.com",
+    "depends": ["hr", "account", "hr_payroll"],
+    "data": [
+        "security/employee_advance_salary_security.xml",
+        "security/ir.model.access.csv",
+        "data/salary_rule_data.xml",
+        "views/employee_advance_salary.xml",
+        "views/hr_job.xml",
+        "report/employee_advance_salary_report.xml",
+    ],
+    "installable": True,
+    "application": False,
 }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ lbc_employee_advance_salary/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "LBC Employee Advance Salary Requests",
+    "version": "1.0.0",
+    "license": "Other proprietary",
+    "category": "Human Resources",
+    "summary": "Employee Advance Salary Requests and Workflow - Integrated with Accounting",
+    "description": """Employee Advance Salary Requests and Workflow - Integrated with Accounting""",
+    "author": "Probuse Consulting Service Pvt. Ltd.",
+    "website": "www.probuse.com",
+    "depends": ["lbc_employee_advance_salary", "libracycle_process_workflow"],
+    "data": [
+        # "security/employee_advance_salary_security.xml",
+       
+    ],
+
+    "installable": True,
+    "application": False,
+}
+
