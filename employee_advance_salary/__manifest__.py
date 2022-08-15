39c186cb663c72b247cca1fb9ea34eec6f4f53db
Babatope Ajepe <ajepebabatope@gmail.com>
2022-08-15 15:18:37 +0100

    [ADD] add libracycle_process_workflow

--- employee_advance_salary/__manifest__.py
+++ employee_advance_salary/__manifest__.py
@@ -3,36 +3,35 @@
 # Part of Probuse Consulting Service Pvt Ltd. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Employee Advance Salary Requests',
-    'version': '5.5.1',
-    'price': 49.0,
-    'currency': 'EUR',
-    'license': 'Other proprietary',
+    "name": "Employee Advance Salary Requests",
+    "version": "5.5.1",
+    "price": 49.0,
+    "currency": "EUR",
+    "license": "Other proprietary",
     # 'live_test_url': 'https://youtu.be/foSt6CHlrgA',
-    'live_test_url': 'https://probuseappdemo.com/probuse_apps/employee_advance_salary/891',#'https://youtu.be/WJ0c0AXXr0E',
-    'category': 'Human Resources',
-    'summary': 'Employee Advance Salary Requests and Workflow - Integrated with Accounting',
-    'description': """
+    "live_test_url": "https://probuseappdemo.com/probuse_apps/employee_advance_salary/891",  #'https://youtu.be/WJ0c0AXXr0E',
+    "category": "Human Resources",
+    "summary": "Employee Advance Salary Requests and Workflow - Integrated with Accounting",
+    "description": """
 Employee Advance Salary Requests:
 Employee advance salary
 Employee advance salary request
             """,
-    'author': 'Probuse Consulting Service Pvt. Ltd.',
-    'website': 'www.probuse.com',
-    'depends': ['hr', 'account', 'hr_payroll'],
-    'images': ['static/description/img2.jpg'],
-    'data': ['security/employee_advance_salary_security.xml',
-             'security/ir.model.access.csv',
-             # 'data/salary_rule_data.xml',
-             'views/employee_advance_salary.xml',
-             'views/hr_job.xml',
-             'report/employee_advance_salary_report.xml'
-             ],
-    'demo': [
-       'data/salary_rule_data.xml'
+    "author": "Probuse Consulting Service Pvt. Ltd.",
+    "website": "www.probuse.com",
+    "depends": ["hr", "account", "hr_payroll"],
+    "images": ["static/description/img2.jpg"],
+    "data": [
+        "security/employee_advance_salary_security.xml",
+        "security/ir.model.access.csv",
+        # 'data/salary_rule_data.xml',
+        "views/employee_advance_salary.xml",
+        "views/hr_job.xml",
+        "report/employee_advance_salary_report.xml",
     ],
-    'installable': True,
-    'application': False,
+    "demo": ["data/salary_rule_data.xml"],
+    "installable": True,
+    "application": False,
 }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
