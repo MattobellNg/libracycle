48fe7735495a05ef9d99cd93df679c55500e56ee
Babatope Ajepe <ajepebabatope@gmail.com>
2022-08-17 11:10:29 +0100

    [UPD] update

--- lbc_employee_advance_salary/__manifest__.py
+++ lbc_employee_advance_salary/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "LBC Employee Advance Salary Requests",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "license": "Other proprietary",
     "category": "Human Resources",
     "summary": "Employee Advance Salary Requests and Workflow - Integrated with Accounting",
@@ -10,11 +10,9 @@
     "depends": ["employee_advance_salary", "libracycle_process_workflow"],
     "data": [
         # "security/employee_advance_salary_security.xml",
-        "views/employee_advance_salary.xml"
-       
+        "data/mail_template.xml",
+        "views/employee_advance_salary.xml",
     ],
-
     "installable": True,
     "application": False,
 }
-
