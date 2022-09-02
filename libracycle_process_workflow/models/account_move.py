f837012c3e48c50af802f4db95d713d2feaea5e5
Babatope Ajepe <ajepebabatope@gmail.com>
2022-09-02 15:24:07 +0100

    [UPD] update

--- libracycle_process_workflow/models/account_move.py
+++ libracycle_process_workflow/models/account_move.py
@@ -79,6 +79,7 @@ class AccountMove(models.Model):
                     "email"
                 )
             )
+            email_from = self.env.user.partner_id.email
             mail_template = self.env.ref(
                 "libracycle_process_workflow.libracycle_mail_template_move"
             )
@@ -126,14 +127,14 @@ class AccountMove(models.Model):
         res = urljoin(base_url, "?%s#%s" % (urlencode(query), urlencode(fragment)))
         return res
 
-    @api.depends('company_id', 'invoice_filter_type_domain')
-    def _compute_suitable_journal_ids(self):
-        for m in self:
-            journal_type = m.invoice_filter_type_domain or 'general'
-            company_id = m.company_id.id or self.env.company.id
-            domain = [('company_id', '=', company_id), ('type', '=', journal_type)]
-            domain = []
-            m.suitable_journal_ids = self.env['account.journal'].search(domain)
+    # @api.depends('company_id', 'invoice_filter_type_domain')
+    # def _compute_suitable_journal_ids(self):
+    #     for m in self:
+    #         journal_type = m.invoice_filter_type_domain or 'general'
+    #         company_id = m.company_id.id or self.env.company.id
+    #         domain = [('company_id', '=', company_id), ('type', '=', journal_type)]
+    #         domain = []
+    #         m.suitable_journal_ids = self.env['account.journal'].search(domain)
 
 
 
