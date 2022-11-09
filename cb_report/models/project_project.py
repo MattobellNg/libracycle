6d9129059bfe2fca072806fb572780a571f8851d
Dhaval Taurani <dhaval.tr@vperfectcs.com>
2022-11-09 19:36:25 +0530

    [FIX]calculation issues resolves

--- cb_report/models/project_project.py
+++ cb_report/models/project_project.py
@@ -42,7 +42,7 @@ class Project(models.Model):
     customer_vat = fields.Integer(string="VAT(N)")
     customer_total_invoice_value = fields.Integer(string="Total Invoice Value(N)")
     customer_invoice_paid = fields.Integer(string="Paid")
-    customer_invoice_unpaid = fields.Integer(string="Not Paid")
+    customer_invoice_unpaid = fields.Integer(string="Not Paid") 
 
     def action_cb_report(self):
         for rec in self:
@@ -66,81 +66,83 @@ class Project(models.Model):
 
     @api.depends('lib_project_com')
     def _compute_process(self):        
-        print ('___ self : ', self);
         for rec in self:
             total_duty=total_shipping_charge=total_terminal_charge=total_nafdac=total_son=total_agency=total_transportation=total_others = 0 
             customer_total_duty=customer_total_shipping_charge=customer_total_terminal_charge=customer_total_nafdac=customer_total_son=customer_total_agency=customer_total_transportation=customer_total_others=invoice_untaxed_value=tax_move_line_value=paid_amount=unpaid_amount=total_invoice_value= 0 
-            if rec.job_vendor_bill_ids:
-                for v in rec.job_vendor_bill_ids:
-                    for d in v.invoice_line_ids.filtered(lambda l: l.product_id.product_duty == True):
+            purchase_types = self.env['account.move'].get_purchase_types()
+            domain = [
+                ('move_id.state', '=', 'posted'),
+                ('move_id.move_type', 'in', purchase_types),
+                ('analytic_account_id', 'in', rec.analytic_account_id.ids)
+            ]
+            groups = self.env['account.move.line'].search(domain)
+            move_ids = []
+            for m in groups:
+                move_ids.append(m.move_id)
+            if move_ids:
+                for v in list(set(move_ids)):
+                    for d in v.invoice_line_ids.filtered(lambda l: l.product_id.product_duty == True and l.analytic_account_id.id == rec.analytic_account_id.id):
                         subtotal_duty_value = d.price_subtotal or 0.0
                         total_duty += subtotal_duty_value
-                    for sh in v.invoice_line_ids.filtered(lambda l: l.product_id.shipping_charge == True):
+                    for sh in v.invoice_line_ids.filtered(lambda l: l.product_id.shipping_charge == True and l.analytic_account_id.id == rec.analytic_account_id.id):
                         subtotal_shipping_value = sh.price_subtotal or 0.0
                         total_shipping_charge += subtotal_shipping_value
-                    for t in v.invoice_line_ids.filtered(lambda l: l.product_id.terminal_charge == True):                        
+                    for t in v.invoice_line_ids.filtered(lambda l: l.product_id.terminal_charge == True and l.analytic_account_id.id == rec.analytic_account_id.id):                        
                         subtotal_terminal_value = t.price_subtotal or 0.0
                         total_terminal_charge += subtotal_terminal_value
-                    for n in v.invoice_line_ids.filtered(lambda l: l.product_id.nafdac == True):
+                    for n in v.invoice_line_ids.filtered(lambda l: l.product_id.nafdac == True and l.analytic_account_id.id == rec.analytic_account_id.id):
                         subtotal_nafdac_value = n.price_subtotal or 0.0
                         total_nafdac += subtotal_nafdac_value
-                    for s in v.invoice_line_ids.filtered(lambda l: l.product_id.son == True):
+                    for s in v.invoice_line_ids.filtered(lambda l: l.product_id.son == True and l.analytic_account_id.id == rec.analytic_account_id.ids):
                         subtotal_son_value = s.price_subtotal or 0.0
                         total_son += subtotal_son_value
-                    for a in v.invoice_line_ids.filtered(lambda l: l.product_id.agency == True):
+                    for a in v.invoice_line_ids.filtered(lambda l: l.product_id.agency == True and l.analytic_account_id.id == rec.analytic_account_id.id):
                         subtotal_agency_value = a.price_subtotal or 0.0
                         total_agency += subtotal_agency_value
-                    for tr in v.invoice_line_ids.filtered(lambda l: l.product_id.transportation == True):
+                    for tr in v.invoice_line_ids.filtered(lambda l: l.product_id.transportation == True and l.analytic_account_id.id == rec.analytic_account_id.id):
                         subtotal_transportation_value = tr.price_subtotal or 0.0
                         total_transportation += subtotal_transportation_value
-                    for o in v.invoice_line_ids.filtered(lambda l: l.product_id.others == True):
+                    for o in v.invoice_line_ids.filtered(lambda l: l.product_id.others == True and l.analytic_account_id.id == rec.analytic_account_id.id):
                         subtotal_others_value = o.price_subtotal or 0.0
                         total_others += subtotal_others_value
             if rec.job_invoice_ids:
                 for i in rec.job_invoice_ids:
-                    print ('___ i : ', i);
-                    for d in i.invoice_line_ids.filtered(lambda l: l.product_id.product_duty == True):
+                    for d in i.invoice_line_ids.filtered(lambda l: l.product_id.product_duty == True and l.analytic_account_id.id == rec.analytic_account_id.id):
                         subtotal_customer_duty_value = d.price_subtotal or 0.0
                         customer_total_duty += subtotal_customer_duty_value
-                    for s in  i.invoice_line_ids.filtered(lambda l: l.product_id.shipping_charge == True):
+                    for s in  i.invoice_line_ids.filtered(lambda l: l.product_id.shipping_charge == True and l.analytic_account_id.id == rec.analytic_account_id.id):
                         subtotal_customer_shipping_charge = s.price_subtotal or 0.0
                         customer_total_shipping_charge += subtotal_customer_shipping_charge
-                    for t in i.invoice_line_ids.filtered(lambda l: l.product_id.terminal_charge == True):
+                    for t in i.invoice_line_ids.filtered(lambda l: l.product_id.terminal_charge == True and l.analytic_account_id.id == rec.analytic_account_id.id):
                         subtotal_customer_terminal_charge = t.price_subtotal or 0.0
                         customer_total_terminal_charge += subtotal_customer_terminal_charge
-                    for n in i.invoice_line_ids.filtered(lambda l: l.product_id.nafdac == True):
+                    for n in i.invoice_line_ids.filtered(lambda l: l.product_id.nafdac == True and l.analytic_account_id.id == rec.analytic_account_id.id):
                         subtotal_customer_nafdac = n.price_subtotal or 0.0
                         customer_total_nafdac += subtotal_customer_nafdac
-                    for so in  i.invoice_line_ids.filtered(lambda l: l.product_id.son == True):
+                    for so in  i.invoice_line_ids.filtered(lambda l: l.product_id.son == True and l.analytic_account_id.id == rec.analytic_account_id.id):
                         subtotal_customer_son = so.price_subtotal or 0.0
                         customer_total_son += subtotal_customer_son
-                    for a in i.invoice_line_ids.filtered(lambda l: l.product_id.agency == True):
+                    for a in i.invoice_line_ids.filtered(lambda l: l.product_id.agency == True and l.analytic_account_id.id == rec.analytic_account_id.id):
                         subtotal_customer_agency = a.price_subtotal or 0.0
                         customer_total_agency += subtotal_customer_agency
-                    for tr in i.invoice_line_ids.filtered(lambda l: l.product_id.transportation == True):
+                    for tr in i.invoice_line_ids.filtered(lambda l: l.product_id.transportation == True and l.analytic_account_id.id == rec.analytic_account_id.id):
                         subtotal_customer_transportation = tr.price_subtotal or 0.0
                         customer_total_transportation += subtotal_customer_transportation
-                    for o in i.invoice_line_ids.filtered(lambda l: l.product_id.others == True):
+                    for o in i.invoice_line_ids.filtered(lambda l: l.product_id.others == True and l.analytic_account_id.id == rec.analytic_account_id.id):
                         subtotal_customer_others = o.price_subtotal or 0.0
                         customer_total_others += subtotal_customer_others                    
                     invoice_untaxed_value += i.amount_untaxed
-                    for tx in i.invoice_line_ids.filtered(lambda l: l.tax_ids):
+                    for tx in i.invoice_line_ids.filtered(lambda l: l.tax_ids and l.analytic_account_id.id == rec.analytic_account_id.id):
                         tax_move_line_value += tx.price_subtotal or 0.0
                     if invoice_untaxed_value or tax_move_line_value:
                         total_invoice_value = invoice_untaxed_value + tax_move_line_value
                     if i.tax_totals_json or i.amount_residual:
                         str_to_dict = json.loads(i.tax_totals_json)
                         cal_unpaid_amount = (i.amount_residual if i.state == "posted" else str_to_dict.get("amount_total"))
-                        print ('___ cal_unpaid_amount : ', cal_unpaid_amount);
                         paid_amount += (str_to_dict.get("amount_total") - cal_unpaid_amount)
-                        print ('___ paid_amount : ', paid_amount);
                         unpaid_amount += cal_unpaid_amount
-                        print ('___ unpaid_amount : ', unpaid_amount);
 
 
-
-            print ('___ Total paid_amount : ', paid_amount);
-            print ('___ Total unpaid_amount : ', unpaid_amount);
             # value assigned to vendor bills
             rec.project_product_duty = total_duty
             rec.project_shipping_charge = total_shipping_charge
