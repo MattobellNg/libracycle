2729363b32067c6622851ba8b11683d7bead0fe3
Babatope Ajepe <ajepebabatope@gmail.com>
2023-02-06 10:07:33 +0100

    [UPD] update

--- customize_vpcs/__manifest__.py
+++ customize_vpcs/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'libra circle Customization',
-    'version': '15.0.1.2.9',
+    'version': '15.0.1.3.1',
     'author': 'VperfectCS',
     'website': 'http://www.vperfectcs.com',
     'description': """
