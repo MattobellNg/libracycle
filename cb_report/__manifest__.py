54065912d76524b4ee2712591de45deedc9c8a5c
Hamza <hamza.ilyaaaas@gmail.com>
2023-06-09 15:24:18 +0500

    [UPD] cb_report

--- cb_report/__manifest__.py
+++ cb_report/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name' : 'C&B Report',
-    'version': '15.0.1.0.1',
+    'version': '15.0.1.0.2',
     'author': 'VperfectCS',
     'website': 'http://www.vperfectcs.com',
     'description': """
