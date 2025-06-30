dea24915b08ec314f0e2a569644e6285b3c686ee
TheEVE001 <167767076+TheEVE001@users.noreply.github.com>
2025-06-30 17:05:50 +0100

    Budget Variance

---
+++ ng_extend_budget/ng_extend_budget/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Budget Variance',
+    'version': '0.3',
+    'category': 'Accounting',
+    'author': 'MOB - Ifeanyi Nneji',
+    'website': 'https://www.mattobell.net/',
+    'summary': 'Extends account budget with variance calculation',
+    'depends': ['account_budget'],
+    'data': [
+        'views/account_budget_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
